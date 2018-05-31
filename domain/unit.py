8cc100edfd6a0bd5f3f684c0222ac8c56e47d02b
Intey <ziexe0@gmail.com>
2018-05-31 19:56:12 +0300

    [fix] setting health

--- domain/unit.py
+++ domain/unit.py
@@ -8,21 +8,17 @@ class Unit:
         self._health = health
         self.alive = self.health > 0
 
-    def health():
-        doc = "The health property."
-        def fget(self):
-            return self._health
-        def fset(self, value):
+    @property
+    def health(self):
+        return self._health
+
+    @health.setter
+    def health(self, value):
+        if value <= 0:
+            self.alive = False
+            self._health = 0
+        else:
             self._health = value
-            if value <= 0:
-                self.alive = False
-        def fdel(self):
-            del self._health
-        return locals()
-    health = property(**health())
 
     def hit(self, power):
         self.health -= power
-        if self.health <= 0:
-            self.health = 0
-            self.alive = False
