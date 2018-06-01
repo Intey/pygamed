9df0d06fb5070e4540df35e35b90329a143f8a6b
Intey <ziexe0@gmail.com>
2018-06-01 08:12:14 +0300

    [ref] correct naming of file

--- actors/__init__.py
+++ actors/__init__.py
@@ -4,4 +4,4 @@ from .bear_factory import BearFactory
 from .trap_factory import TrapFactory
 from .player_actor import PlayerActor
 # from .sticks_factory import SticksFactory
-from .mappositioner import MapPositioner
+from .map_positioner import MapPositioner
